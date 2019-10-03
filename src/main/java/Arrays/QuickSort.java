54c1ba5545b5a0c847443cebb1ae082b3140cefa
HatimLokhandwala <htm.lokhandwala@gmail.com>
2019-10-03 12:27:47 +0530

    cleaning up quick sort program

--- src/main/java/Arrays/QuickSort.java
+++ src/main/java/Arrays/QuickSort.java
@@ -2,10 +2,11 @@ package Arrays;
 
 /**
  * Created by hatim.lokhandwala on 20/07/19.
+ * Implementation of Quick Sort
+ * First element from a given range is chosen as the pivot
  */
 public class QuickSort {
 	static int partition(int[] arr, int left, int right) {
-//		int pivotIndex = left;
 		int pivot = arr[left];
 		int index = left;
 		for(int i = left + 1; i <= right; i++) {
@@ -22,24 +23,19 @@ public class QuickSort {
 		return index;
 	}
 
-//4 	2 3 6 7
-
 	static void sort(int[] arr, int left, int right) {
 		if(left <= right) {
 			int index = partition(arr, left, right);
 			sort(arr, left, index - 1);
 			sort(arr, index + 1, right);
 		}
-
 	}
+
 	public static void main(String[] args) {
 		int[] arr = {2, 1, 34, 23, 60, 10};
-
 		sort(arr, 0, arr.length - 1);
 		for(int a: arr) {
 			System.out.println(a);
-
 		}
-
 	}
 }
